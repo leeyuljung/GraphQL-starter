d7760157d285a4644f7ad62413038f96f999172c
leeyuljung <rita810605@gmail.com>
2022-03-27 21:01:37 +0800

    feat: 簡易 user Query

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+const users = [
+    {
+        id: 1,
+        name: 'Yuljung',
+        age: 25
+    },
+    {
+        id: 2,
+        name: 'Panda',
+        age: 16
+    },
+    {
+        id: 3,
+        name: 'Uber',
+        age: 33
+    }
+]
+
 // GraphQL Schema 定義
 const typeDefs = gql`
     type Query {
-        "A simple type for getting started!"
         hello: String
+        "取得當前使用者"
+        me: User
+    }
+
+    """
+    使用者資訊
+    """
+    type User {
+        "識別碼"
+        id: ID
+        "名字"
+        name: String
+        "年齡"
+        age: Int
     }
 `;
 
 // Resolvers
 const resolvers = {
     Query: {
-        hello: () => 'world'
+        hello: () => 'world',
+        me: () => users[0]
     }
 }
 
