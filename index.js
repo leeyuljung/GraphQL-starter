0ec42b4b8a84015a0a85b583964b4eeb623e7a63
leeyuljung <rita810605@gmail.com>
2022-04-06 03:21:21 +0800

    feat: 加入查詢使用者的貼文相關資訊

--- index.js
+++ index.js
@@ -27,12 +27,33 @@ const users = [
     }
 ]
 
+const posts = [
+    {
+        id: 1,
+        authorId: 1,
+        title: 'Hello World',
+        content: 'Hello World yeah',
+        likeGiverIds: [2]
+    },
+    {
+        id: 2,
+        authorId: 2,
+        title: 'Good Night',
+        content: 'Gonna go to sleep',
+        likeGiverIds: [2, 3]
+    },
+    {
+        id: 3,
+        authorId: 1,
+        title: 'Hungry moment',
+        content: 'I want eating lots of hamburgers',
+        likeGiverIds: []
+    }
+]
+
 // GraphQL Schema 定義
 const typeDefs = gql`
     type Query {
-        hello: String
-        "取得當前使用者"
-        me: User
         "取得所有使用者"
         users: [User]
         "取得特定使用者"
@@ -51,59 +72,58 @@ const typeDefs = gql`
         age: Int
         "好友"
         friends: [User]
-        "身高"
-        height(unit: HeightUnit = CM): Float
-        "體重"
-        weight(unit: WeightUnit = G): Float
+        "貼文"
+        posts: [Post]
     }
 
     """
-    高度單位
+    貼文
     """
-    enum HeightUnit {
-        "公分"
-        CM
-        "公尺"
-        M
+    type Post {
+        "識別碼"
+        id: ID!
+        "作者"
+        author: User
+        "標題"
+        title: String
+        "內容"
+        content: String
+        "按讚者"
+        likeGivers: [User]
     }
-
-    """
-    重量單位
-    """
-    enum WeightUnit {
-        "公斤"
-        KG
-        "克"
-        G
-    }
-    
 `;
 
 // Resolvers
 const resolvers = {
     Query: {
-        hello: () => 'world',
-        me: () => users[0],
         users: () => users,
-        user: (root, args, context) => {
+        user: (parent, args) => {
             const {name} = args
             return users.find(user => user.name === name)
         }
     },
     User: {
-        friends: (parent, args, context) => {
+        friends: (parent) => {
+            // 從 users 中找出 id 符合 friendIds 的 users
             const {friendIds} = parent
             return users.filter(user => friendIds.includes(user.id))
         },
-        height: (parent, args) => {
-            const {unit} = args
-            if (unit === 'CM') return parent.height
-            else if (unit === 'M') return parent.height / 100
+        posts: (parent) => {
+            // 找出該使用者的所有 posts，所以從所有貼文過濾出 authorId 為 user.id 的貼文
+            const {id} = parent
+            return posts.filter(post => post.authorId === id)
+        }
+    },
+    Post: {
+        author: (parent) => {
+            // 從 users 中找出 id 符合貼文中的 authorId 的 user
+            const {authorId} = parent
+            return users.find(user => user.id === authorId)
         },
-        weight: (parent, args, context) => {
-            const {unit} = args
-            if (unit === 'KG') return parent.weight
-            else if (unit === 'G') return parent.weight * 1000
+        likeGivers: (parent) => {
+            // 從 users 中找出 id 符合 likeGiverIds 的 users
+            const {likeGiverIds} = parent
+            return users.filter(user => likeGiverIds.includes(user.id))
         }
     }
 }
